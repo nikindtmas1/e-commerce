71b0c84c2adc0fc8c5fe1ccc6620827e09df03de
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-06-29 10:47:47 +0300

    Added class names

--- e-commerce/pages/index.js
+++ e-commerce/pages/index.js
@@ -4,12 +4,12 @@ const Home = () => {
   return (
     <>
       HeroBanner
-      <div>
+      <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speacers of many variations</p>
       </div>
 
-      <div>
+      <div className='products-container'>
         {["Product 1", "Product 2"].map(
           (product) => product)}
       </div>
