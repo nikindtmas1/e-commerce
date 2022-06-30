c13144dede5edf5a29b276fda7917160cb30df13
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-06-30 15:32:54 +0300

    Change projectId

--- e-commerce/lib/client.js
+++ e-commerce/lib/client.js
@@ -2,7 +2,7 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-    projectid: 'rfpmlxtj',
+    projectId: 'rfpmlxtj',
     dataset: 'production',
     apiVersion: '2022-06-29',
     useCdn: 'true',
