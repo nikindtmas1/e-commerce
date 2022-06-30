007b3c9388fa8e6165c23a6387c635609db6b35a
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-06-30 15:33:13 +0300

    Update index.js

--- e-commerce/pages/index.js
+++ e-commerce/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import { client } from '../lib/client';
+
 import { FooterBanner, HeroBanner, Product } from '../components';
 
 
-const Home = () => {
-  return (
-    <>
+const Home = ({products, bannerData}) => (
+ 
+    <div>
       <HeroBanner />
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
@@ -13,13 +15,25 @@ const Home = () => {
       </div>
 
       <div className='products-container'>
-        {["Product 1", "Product 2"].map(
-          (product) => product)}
+        {products?.map(
+          (product) => product.name)}
       </div>
 
       <FooterBanner />
-    </>
-  )
+    </div>
+);
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
 }
+
 
 export default Home;
