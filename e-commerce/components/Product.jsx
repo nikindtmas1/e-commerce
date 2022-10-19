3dd98a462acc06790b23af5192cc46e4bd5ac97d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-10-19 14:25:35 +0300

    Added product date

--- e-commerce/components/Product.jsx
+++ e-commerce/components/Product.jsx
@@ -4,9 +4,22 @@ import Link from 'next/link';
 
 import { urlFor } from '../lib/client';
 
-const Product = ({ product }) => {
+const Product = ({ product: {image, name, slug, price} }) => {
   return (
-    <div>Product</div>
+    <div>
+      <Link href={'/product/$(slug.current)'}>
+        <div className='product-card'>
+          <img 
+            src={urlFor(image && image[0])}
+            width={250}
+            height={250}
+            className='product-image'
+          />
+          <p className='produst-name'>{name}</p>
+          <p className='produst-price'>${price}</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
