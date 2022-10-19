eb0d42d2cd431952523a8812c958b6c43a7de83f
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-10-19 11:48:49 +0300

    Make change

--- e-commerce/pages/index.js
+++ e-commerce/pages/index.js
@@ -16,6 +16,7 @@ const Home = ({products, bannerData}) => (
       </div>
 
       <div className='products-container'>
+        {/* {['Product 1', 'Product 2'].map((product) => product)} */}
         {products?.map(
           (product) => product.name)}
       </div>
