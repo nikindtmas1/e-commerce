412ca6aa571909f765ecdb25f6c7f72f93436076
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-10-20 12:11:27 +0300

    Added tags

--- e-commerce/components/FooterBanner.jsx
+++ e-commerce/components/FooterBanner.jsx
@@ -6,7 +6,19 @@ import { urlFor } from '../lib/client';
 
 const FooterBanner = ({ footerBanner }) => {
   return (
-    <div className='footer-banner-container'>FooterBanner</div>
+    <div className='footer-banner-container'>
+      <div className='banner-desc'>
+        <div className='left'>
+          <p>{footerBanner.discount}</p>
+          <h3>{footerBanner.largeText1}</h3>
+          <h3>{footerBanner.largeText2}</h3>
+          <p>{footerBanner.saleTime}</p>
+        </div>
+        <div className='right'>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
