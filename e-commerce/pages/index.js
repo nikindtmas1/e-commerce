40e550bbf07254465ebd7ef0ba0ebf5aedb56680
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-10-20 12:04:54 +0300

    Added parameter

--- e-commerce/pages/index.js
+++ e-commerce/pages/index.js
@@ -22,7 +22,8 @@ const Home = ({products, bannerData}) => (
           (product) => <Product key={product._id} product={product} />)}
       </div>
 
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
+      
     </div>
 );
 
