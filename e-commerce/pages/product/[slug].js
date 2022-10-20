175b1bcdfa07c0d5e333e95e1d6bc38a0c84ab15
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-10-20 15:04:42 +0300

    Create product logic

--- e-commerce/pages/product/[slug].js
+++ e-commerce/pages/product/[slug].js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from 'react';
 
-const [slug] = () => {
+import { client, urlFor } from '../../lib/client';
+
+
+const ProductDetails = ({product, products}) => {
+    // const { image, name, details, price } = product;
   return (
-    <div>[slug]</div>
+    <div>
+        <div className='product-detail-container'>
+            <div>
+                <div className='image-container'>
+                    <img src=''
+                    // {urlFor(image && image[0])}
+                     />
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
-export default [slug]
+export const getStaticPaths = async () => {
+    const query = `*[_type == "product"]
+    {
+        slug {
+            current
+        }
+    }` ;
+
+    const products = await client.fetch(query);
+
+    const paths = products.map((product) => ({
+        params : {
+            slug : product.slug.current
+        }
+    }));
+
+    return {
+        paths,
+        fallback: 'blocking'
+    }
+}
+
+export const getStaticProps = async ({params: {slug}}) => {
+    const query = `*[_type == "product" && sulg.current == '${slug}'][0]`;
+    const productQuery = '*[_type == "product"]';
+
+    const product = await client.fetch(query);
+    const products = await client.fetch(productQuery);
+  
+    return {
+      props: { products, product }
+    }
+  }
+
+export default ProductDetails
