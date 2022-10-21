6e333bad8d58946b7c96011e1dab7f5cd563a3c4
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-10-21 16:31:50 +0300

    Import useState

--- e-commerce/pages/product/[slug].js
+++ e-commerce/pages/product/[slug].js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import { client, urlFor } from '../../lib/client';
 
@@ -9,6 +9,7 @@ import { Product } from '../../components';
 
 const ProductDetails = ({product, products}) => {
     const { image, name, details, price } = product[0];
+    const [index, setIndex]= useState(0) 
   return (
     <div>
         <div className='product-detail-container'>
@@ -70,7 +71,7 @@ const ProductDetails = ({product, products}) => {
         <div className='maylike-products-wrapper'>
             <h2>You may also like</h2>
             <div className='marquee'>
-                <div className='maylike-products-container'>
+                <div className='maylike-products-container track'>
                     {products.map((item) => (
                         <Product key={item._id} product={item} />
                     ))}
