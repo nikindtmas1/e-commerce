5429640d7ce8688b5b4032f96e9bb3084a4e8bf7
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-10-21 16:46:32 +0300

    Make change

--- e-commerce/pages/product/[slug].js
+++ e-commerce/pages/product/[slug].js
@@ -9,7 +9,7 @@ import { Product } from '../../components';
 
 const ProductDetails = ({product, products}) => {
     const { image, name, details, price } = product[0];
-    const [index, setIndex]= useState(0) 
+    const [index, setIndex]= useState(0);
   return (
     <div>
         <div className='product-detail-container'>
@@ -44,7 +44,7 @@ const ProductDetails = ({product, products}) => {
                 </div>
                 <h4>Details: </h4>
                 <p>{details}</p>
-                <p className='price'>$ {price}</p>
+                <p className='price'>${price}</p>
                 <div className='quantity'>
                     <h3>Quantity:</h3>
                     <p className='quantity-desc'>
