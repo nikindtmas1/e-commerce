b78fcbfbf6ad0d3cb8a2255911ae98549d660ff9
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-10-21 16:25:53 +0300

    Added more items for likes

--- e-commerce/pages/product/[slug].js
+++ e-commerce/pages/product/[slug].js
@@ -4,6 +4,8 @@ import { client, urlFor } from '../../lib/client';
 
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 
+import { Product } from '../../components';
+
 
 const ProductDetails = ({product, products}) => {
     const { image, name, details, price } = product[0];
@@ -67,6 +69,13 @@ const ProductDetails = ({product, products}) => {
         </div>
         <div className='maylike-products-wrapper'>
             <h2>You may also like</h2>
+            <div className='marquee'>
+                <div className='maylike-products-container'>
+                    {products.map((item) => (
+                        <Product key={item._id} product={item} />
+                    ))}
+                </div>
+            </div>
         </div>
     </div>
   )
