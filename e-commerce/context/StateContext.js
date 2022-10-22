fe42e0781d966382ec6ea751a5eab767d7e1161f
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-10-22 15:13:44 +0300

    Added states

--- e-commerce/context/StateContext.js
+++ e-commerce/context/StateContext.js
@@ -6,5 +6,26 @@ import { toast } from 'react-hot-toast';
 const Context = createContext();
 
 export const StateContext = ({ children }) => {
-    const [ showCart, setshowCart ] = useState(false)
+    const [ showCart, setShowCart ] = useState(false);
+
+    const [cartItems, setCartItems] = useState();
+
+    const [totalPrice, setTotalPrice] = useState();
+
+    const [totalQuantities, setTotalQuantities] = useState();
+
+    const [qty, setQty] = useState(1);
+
+    return (
+        <Context.Provider value={{
+            showCart,
+            cartItems,
+            totalPrice,
+            totalQuantities,
+            qty
+        }}
+        >
+            {children}
+        </Context.Provider>
+    );
 }
