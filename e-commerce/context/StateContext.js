a510c1f88e0352956add382993824cbcffaf2117
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-10-22 14:55:20 +0300

    Create context folder

---
+++ e-commerce/context/StateContext.js
@@ -0,0 +1,10 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+import { toast } from 'react-hot-toast';
+
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+    const [ showCart, setshowCart ] = useState(false)
+}
