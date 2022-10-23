3849eb797f4c1070c13b3f45ed5aa5b60e94c9c8
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-10-23 11:50:12 +0300

    Added qty logic for dec

--- e-commerce/context/StateContext.js
+++ e-commerce/context/StateContext.js
@@ -17,6 +17,19 @@ export const StateContext = ({ children }) => {
 
     const [qty, setQty] = useState(1);
 
+    const incQty = () => {
+        setQty((prevQty) => prevQty + 1)
+    }
+
+    const decQty = () => {
+        setQty((prevQty) => {
+            
+            if(prevQty - 1 < 1)  return 1;
+
+            return prevQty - 1
+        })
+    }
+
     return (
         <Context.Provider value={{
             showCart,
