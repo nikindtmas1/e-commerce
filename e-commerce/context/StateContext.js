8ea34818715458273ffa7d13782051e5df66cd35
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-10-23 11:55:08 +0300

    format page

--- e-commerce/context/StateContext.js
+++ e-commerce/context/StateContext.js
@@ -24,7 +24,7 @@ export const StateContext = ({ children }) => {
     const decQty = () => {
         setQty((prevQty) => {
 
-            if(prevQty - 1 < 1)  return 1;
+            if (prevQty - 1 < 1) return 1;
 
             return prevQty - 1
         })
