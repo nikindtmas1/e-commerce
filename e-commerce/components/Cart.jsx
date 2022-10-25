0b4099172224392ef50b4453ce336687057db48d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-10-25 16:25:05 +0300

    Import moduls from react-icons

--- e-commerce/components/Cart.jsx
+++ e-commerce/components/Cart.jsx
@@ -2,6 +2,8 @@ import React, {useRef} from 'react';
 
 import Link from 'next/link';
 
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
+
 const Cart = () => {
   return (
     <div>Cart</div>
