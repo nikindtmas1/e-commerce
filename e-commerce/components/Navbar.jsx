1ceb4020d3d09b95f0f03e2e124a3fb4066983f1
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-10-25 16:19:44 +0300

    Added functionalities for button

--- e-commerce/components/Navbar.jsx
+++ e-commerce/components/Navbar.jsx
@@ -10,16 +10,17 @@ import { useStateContext } from '../context/StateContext';
 
 
 const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
   return (
     <div className='navbar-container'>
       <p className='logo'>
         <Link href='/'>JSM Headphones</Link>
       </p>
-      <button type='button' className='cart-icon' onClick='ok'>
+      <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
-        <span className='cart-item-qty'>1</span>
+        <span className='cart-item-qty'>{totalQuantities}</span>
       </button>
-      <Cart />
+      {showCart && <Cart />}
     </div>
   )
 }
