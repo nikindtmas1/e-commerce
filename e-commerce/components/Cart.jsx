6f13874678e5efdbb8833eee06378218fb6d0fae
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-11-15 13:54:23 +0200

    Add item quantity

--- e-commerce/components/Cart.jsx
+++ e-commerce/components/Cart.jsx
@@ -52,7 +52,7 @@ const Cart = () => {
                             }>
                                 <AiOutlineMinus />
                             </span>
-                            <span className='num' onClick=''>0</span>
+                            <span className='num' onClick=''>{item.quantity}</span>
                             <span className='plus' onClick={() => 
                             toggleCartItemQuantity(item._id, 'inc')
                             }>
