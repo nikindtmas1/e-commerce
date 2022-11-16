a91b3d71242bd034331a65ca8be5bce1848855c1
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-11-16 09:52:39 +0200

    Create onRemove function

--- e-commerce/context/StateContext.js
+++ e-commerce/context/StateContext.js
@@ -44,10 +44,18 @@ export const StateContext = ({ children }) => {
         toast.success(`${qty} ${product.name} added to the cart.`);
     };
 
+    const onRemove = (product) => {
+        foundProduct = cartItems.find((item) => item._id === id);
+        const newCartItems = cartItems.filter((item) => item._id !== id);
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price * foundProduct.quantity);
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - foundProduct.quantity);
+        setCartItems(newCartItems);
+    };
+
     const toggleCartItemQuantity = (id, value) => {
         foundProduct = cartItems.find((item) => item._id === id);
         index = cartItems.findIndex((product) => product._id === id);
-        const newCartItems = cartItems.filter((item) => item._id !== id)
+        const newCartItems = cartItems.filter((item) => item._id !== id);
 
         if(value === 'inc'){
             setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 }]);
@@ -91,6 +99,7 @@ export const StateContext = ({ children }) => {
             decQty,
             onAdd,
             toggleCartItemQuantity,
+            onRemove,
         }}
         >
             {children}
