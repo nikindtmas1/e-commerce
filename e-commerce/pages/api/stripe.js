db0f23c157512d027f8b672606a366989d00957a
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-11-18 15:02:05 +0200

    Create function handler

--- e-commerce/pages/api/stripe.js
+++ e-commerce/pages/api/stripe.js
@@ -1,3 +1,5 @@
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY,);
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+
+export default async function handler(req, res)
